c9023f72242db3808fc1a6626dde9a5e75ef9f7d
Ziming Mao <ziming.mao@yale.edu>
2020-05-02 23:55:28 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,13 @@
 def find_max_value(array)
   # Add your solution here
+  max = array[0]
+
+  length = array.size
+  i = 0
+  size.times do
+    if array[i] > max 
+      max = array[i]
+    end
+    i ++
+  end
 end
